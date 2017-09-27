32d6ec533827e673e8b9062bbabd747d38c9c3a4
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-27 21:32:01 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,28 +3,42 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
+        name: "Lord Montague",
+        age: "53"
         },
       :matriarch => {
         #your key/value pairs here
+        name: "Lady Montague",
+        age: "54"
         },
       :hero => {
         #your key/value pairs here
+        name: "Romeo",
+        age: ""
+        status: "alive"
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
+        name: "",
+        age: ""
         },
       :matriarch => {
         #your key/value pairs here
+        name: "",
+        age: ""
         },
       :heroine => {
         #your key/value pairs here
+        name: "",
+        age: ""
+        status: ""
         },
       :heroine_friends => []
    }
   }
 
-  
+
 end
